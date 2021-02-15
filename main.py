a60f540f054e98ad6787826bf153b5ba95c5470c
Loralee Ryan <lolerary@Loralees-Air.home>
2021-02-15 09:08:18 -0500

    added sessions

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 
-from flask import Flask, flash, jsonify, redirect, render_template, request
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 import random
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 import mysql.connector
 from mysql.connector.constants import ClientFlag
@@ -38,35 +44,31 @@ def after_request(response):
     return response
 
 # Initialize a number for a unique database
-user_id = 0
-user_slide = ''
-user_slides = ''
-str_user_id = ''
+#[session['user_id']] = 0
+#session['user_slide'] = ''
+#session['user_slides'] = ''
+#session['str_user_id'] = ''
 
 
 @app.route("/", methods=["GET", "POST"])
 def start():
     # initalize all global variables
-    global user_id
-    global user_slide
-    global user_slides
-    global str_user_id
     if request.method == "POST":
 
         #create a table of slides unique to a user. Insert the slides the user selected into the table.
-        user_slides = "slides"+str(user_id)
-        cursor.execute("create table "+user_slides+" (slide VARCHAR(50))")
+        session['user_slides'] = "slides"+str(session['user_id'])
+        cursor.execute("create table "+session['user_slides']+" (slide VARCHAR(50))")
         conn.commit()
         for value in request.form:
             if value != 'selectall':
-                cursor.execute("insert into "+user_slides+" (slide) VALUES ('"+str(user_id)+value+"')")
+                cursor.execute("insert into "+session['user_slides']+" (slide) VALUES ('"+str(session['user_id'])+value+"')")
                 conn.commit()
 
         return redirect("quiz")
     else:
 
         #create a unique id for each user
-        user_id = random.randint(1000,9999)
+        session['user_id'] = random.randint(1000,9999)
 
         return render_template("start.html")
 
@@ -77,15 +79,18 @@ def quiz():
 
     #get slides that the user selected and put them in a list
     slides = []
-    str_user_id = str(user_id) +'%'
-    cursor.execute("select * from "+user_slides+" where slide like '"+str_user_id+"'")
+    session['str_user_id'] = str(session['user_id']) +'%'
+    print (session['str_user_id'])
+    query = ("select * from "+session['user_slides']+" where slide like '"+session['str_user_id']+"'")
+    print(query)
+    cursor.execute("select * from "+session['user_slides']+" where slide like '"+session['str_user_id']+"'")
     get_slides = cursor.fetchall()
 
     for slide in get_slides:
         slides.append(slide['slide'])
 
     #get tables already created.
-    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+str_user_id+"'")
+    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+session['str_user_id']+"'")
     tables = cursor.fetchall()
 
     print(slides)
@@ -100,23 +105,23 @@ def quiz():
     #direct to final score if <<something>> otherwise render the next template in the list.
     if request.form.get("template") is None:
         if len(slides) > 0:
-            slides[0] = slides[0].split(str(user_id),1)[1]
+            slides[0] = slides[0].split(str(session['user_id']),1)[1]
             return render_template(slides[0])
         return redirect("final_score")
 
-    #gets slide id, remove .html to make sql friendly, add user_id
+    #gets slide id, remove .html to make sql friendly, add session['user_id']
     slide_id = (request.form.get("template"))
     sql_slide_id = slide_id.split('.',1)[0]
-    user_slide = str(user_id) + sql_slide_id
+    session['user_slide'] = str(session['user_id']) + sql_slide_id
 
     #gets user answers
     for key in request.form:
         answers[key] = (request.form.get(key))
 
     del answers["template"]
-    cursor.execute("drop table if exists "+user_slide)
+    cursor.execute("drop table if exists "+session['user_slide'])
     conn.commit()
-    cursor.execute("create table "+user_slide+" (correct VARCHAR(50), user_input VARCHAR(50), is_correct VARCHAR(20), color VARCHAR(20))")
+    cursor.execute("create table "+session['user_slide']+" (correct VARCHAR(50), user_input VARCHAR(50), is_correct VARCHAR(20), color VARCHAR(20))")
     conn.commit()
 
     color = ''
@@ -129,20 +134,20 @@ def quiz():
         else:
             is_correct = 'Incorrect!'
             color = '#800000'
-        cursor.execute("insert into "+user_slide+" (correct, user_input, is_correct, color) VALUES ('"+a+"', '"+answers[a]+"', '"+is_correct+"', '"+color+"')")
+        cursor.execute("insert into "+session['user_slide']+" (correct, user_input, is_correct, color) VALUES ('"+a+"', '"+answers[a]+"', '"+is_correct+"', '"+color+"')")
         conn.commit()
 
     img_id = slide_id
     img_id = img_id.replace("html", "jpg")
-    cursor.execute("select * from "+user_slide)
+    cursor.execute("select * from "+session['user_slide'])
     answers_to_score = cursor.fetchall()
 
-    return render_template("score.html", slide_id=user_slide, answers_to_score=answers_to_score, img_id=img_id)
+    return render_template("score.html", slide_id=session['user_slide'], answers_to_score=answers_to_score, img_id=img_id)
 
 @app.route("/final_score", methods=["GET", "POST"])
 def final_score():
-    str_user_id ='%' + str(user_id) + '%'
-    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+str_user_id+"'")
+    session['str_user_id'] ='%' + str(session['user_id']) + '%'
+    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+session['str_user_id']+"'")
     tables = cursor.fetchall()
 
     total = 0
@@ -163,7 +168,7 @@ def final_score():
     else:
         score = round(100/total * correct)
 
-    cursor.execute("SELECT table_name from information_schema.tables where table_name like '"+str_user_id+"' ")
+    cursor.execute("SELECT table_name from information_schema.tables where table_name like '"+session['str_user_id']+"' ")
     tables = cursor.fetchall()
     print("final score tables are")
     print(tables)
@@ -172,4 +177,5 @@ def final_score():
             cursor.execute("drop table if exists "+table)
             conn.commit()
     conn.close()
+    session.clear()
     return render_template("final_score.html", score=score)
