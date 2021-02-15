3b53d03f737bfda1d354818a40e7bf8c01c246aa
Loralee Ryan <lolerary@Loralees-Air.home>
2021-02-14 23:22:48 -0500

    got cloud sql working

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 import random
 
+
 import mysql.connector
 from mysql.connector.constants import ClientFlag
 
@@ -15,12 +16,12 @@ config = {
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'server-ca.pem',
     'ssl_cert': 'client-cert.pem',
-    'ssl_key': 'client-key.pem'
+    'ssl_key': 'client-key.pem',
+    'database': 'navyquizdb'
 }
 
 conn = mysql.connector.connect(**config)
-cursor = conn.cursor()
-
+cursor = conn.cursor(buffered=True, dictionary=True)
 
 # Configure application
 app = Flask(__name__)
@@ -35,14 +36,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-
-##drop all tables
-#tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-#tables.pop(0)
-#for t in tables:
-#    table = t['name']
-#    cursor.execute("drop table if exists :table",table=table)
 
 # Initialize a number for a unique database
 user_id = 0
@@ -62,11 +55,11 @@ def start():
 
         #create a table of slides unique to a user. Insert the slides the user selected into the table.
         user_slides = "slides"+str(user_id)
-        cursor.execute("create table :slides (slide VARCHAR(50))", slides=user_slides)
+        cursor.execute("create table "+user_slides+" (slide VARCHAR(50))")
         conn.commit()
         for value in request.form:
             if value != 'selectall':
-                cursor.execute("insert into :slides (slide) VALUES (:value)", slides=user_slides, value=value+str(user_id))
+                cursor.execute("insert into "+user_slides+" (slide) VALUES ('"+str(user_id)+value+"')")
                 conn.commit()
 
         return redirect("quiz")
@@ -74,11 +67,6 @@ def start():
 
         #create a unique id for each user
         user_id = random.randint(1,10000)
-        #tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' and name LIKE :user_id", user_id=user_id)
-        #tables.pop(0)
-        #for t in tables:
-        #    table = t['name']
-        #    cursor.execute("drop table if exists :table",table=table)
 
         return render_template("start.html")
 
@@ -89,44 +77,44 @@ def quiz():
 
     #get slides that the user selected and put them in a list
     slides = []
-    print(user_id)
-    str_user_id ='%' + str(user_id)
-    get_slides = cursor.execute("select * from :slides where slide like :user", slides=user_slides, user=str_user_id)
+    str_user_id ='%' + str(user_id) +'%'
+    cursor.execute("select * from "+user_slides+" where slide like '"+str_user_id+"'")
+    get_slides = cursor.fetchall()
 
     for slide in get_slides:
         slides.append(slide['slide'])
 
-    #get tables already created. This is currently broken because we need to access tables unique to user id.
-    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' and name like '%.html%' and name like :user", user=str_user_id)
+    #get tables already created.
+    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+str_user_id+"'")
+    tables = cursor.fetchall()
 
-    print(tables)
     print(slides)
-
+    print(tables)
     #remove table from slides if it already exists
     for t in tables:
-        table = t['name']
-        table=table
+        table = t['table_name'] + '.html'
         slides.remove(table)
 
     #direct to final score if <<something>> otherwise render the next template in the list.
     if request.form.get("template") is None:
         if len(slides) > 0:
-            slides[0] = slides[0].split('.html',1)[0]
-            return render_template(slides[0]+".html")
+            slides[0] = slides[0].split(str(user_id),1)[1]
+            return render_template(slides[0])
         return redirect("final_score")
 
-    #gets slide id. add user id here?
+    #gets slide id, remove .html to make sql friendly, add user_id
     slide_id = (request.form.get("template"))
-    user_slide = slide_id+str(user_id)
+    sql_slide_id = slide_id.split('.',1)[0]
+    user_slide = str(user_id) + sql_slide_id
 
     #gets user answers
     for key in request.form:
         answers[key] = (request.form.get(key))
 
     del answers["template"]
-    cursor.execute("drop table if exists :table",table=user_slide)
+    cursor.execute("drop table if exists "+user_slide)
     conn.commit()
-    cursor.execute("create table :table (correct VARCHAR(50), user_input VARCHAR(50), is_correct VARCHAR(20), color VARCHAR(20))", table=user_slide)
+    cursor.execute("create table "+user_slide+" (correct VARCHAR(50), user_input VARCHAR(50), is_correct VARCHAR(20), color VARCHAR(20))")
     conn.commit()
 
     color = ''
@@ -139,29 +127,29 @@ def quiz():
         else:
             is_correct = 'Incorrect!'
             color = '#800000'
-        cursor.execute("insert into :table (correct, user_input, is_correct, color) VALUES (:correct, :user_input, :is_correct, :color)",
-        table=user_slide, correct=a, user_input=answers[a], is_correct=is_correct, color=color)
+        cursor.execute("insert into "+user_slide+" (correct, user_input, is_correct, color) VALUES ('"+a+"', '"+answers[a]+"', '"+is_correct+"', '"+color+"')")
         conn.commit()
 
-    print(slide_id)
     img_id = slide_id
     img_id = img_id.replace("html", "jpg")
-    print(img_id)
-    answers_to_score = cursor.execute("select * from :table", table=user_slide)
+    cursor.execute("select * from "+user_slide)
+    answers_to_score = cursor.fetchall()
 
     return render_template("score.html", slide_id=user_slide, answers_to_score=answers_to_score, img_id=img_id)
 
 @app.route("/final_score", methods=["GET", "POST"])
 def final_score():
-    str_user_id ='%' + str(user_id)
-    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' and name like '%.html%' and name like :user", user=str_user_id)
+    str_user_id ='%' + str(user_id) + '%'
+    cursor.execute("SELECT table_name from information_schema.tables where table_name not like 'slides%' and table_name like '"+str_user_id+"'")
+    tables = cursor.fetchall()
 
     total = 0
     correct = 0
 
     for t in tables:
-        table = t['name']
-        ans = cursor.execute("select * from :table", table=table)
+        table = t['table_name']
+        cursor.execute("select * from "+table)
+        ans = cursor.fetchall()
         total += len(ans)
         #print(ans)
         for a in ans:
@@ -173,11 +161,13 @@ def final_score():
     else:
         score = round(100/total * correct)
 
-    tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' and name like :user", user=str_user_id)
-    tables.pop(0)
+    cursor.execute("SELECT table_name from information_schema.tables where table_name like '"+str_user_id+"' ")
+    tables = cursor.fetchall()
+    print("final score tables are")
+    print(tables)
     for t in tables:
-            table = t['name']
-            cursor.execute("drop table if exists :table",table=table)
+            table = t['table_name']
+            cursor.execute("drop table if exists "+table)
             conn.commit()
     conn.close()
     return render_template("final_score.html", score=score)
